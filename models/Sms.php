0cb19d31340343fa55b624c9ea8d3035fbde837f
marcobusacca <marcobusacca01@gmail.com>
2023-07-11 22:02:14 +0200

    [FEAT] function construct Sms

--- models/Sms.php
+++ models/Sms.php
@@ -4,5 +4,15 @@
         private $readNotification;
         private $replyApproval;
         public static $ledColor = "Green";
+
+        // METODI
+            // COSTRUTTORE
+            function __construct(String $sender, String $receiver, String $title, String $content){
+                parent::__construct($sender, $receiver, $title, $content);
+                $this->readNotification = true;
+                $this->replyApproval = true;
+            }
+
+        // FINE METODI
     }
 ?>
