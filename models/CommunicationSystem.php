29ea2920122d19b79f4952ebb13be1eeaece3b9f
marcobusacca <marcobusacca01@gmail.com>
2023-07-11 21:57:23 +0200

    [FEAT] function construct CommunicationSystem

--- models/CommunicationSystem.php
+++ models/CommunicationSystem.php
@@ -6,5 +6,16 @@
         private $title;
         private $content;
         public static $ringtone = "DRIIIN";
+
+        // METODI
+            // COSTRUTTORE
+            function __construct($sender, $receiver, $title, $content){
+                $this->sender = $sender;
+                $this->receiver = $receiver;
+                $this->title = $title;
+                $this->content = $content;
+            }
+
+        // FINE METODI
     }
 ?>
