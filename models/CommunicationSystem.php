7fa37ea92da8ae757068bd20bef73e33d9f83599
marcobusacca <marcobusacca01@gmail.com>
2023-07-11 17:49:26 +0200

    [FEAT] proprieta classe padre CommunicationSystem

--- models/CommunicationSystem.php
+++ models/CommunicationSystem.php
@@ -1,5 +1,10 @@
 <?php
     class CommunicationSystem{
-
+        // PROPRIETA
+        private $sender;
+        private $receiver;
+        private $title;
+        private $content;
+        private static $ringtone = "DRIIIN";
     }
 ?>
