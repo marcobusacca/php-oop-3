ec1d0b7d2059de7822165cf8230cecb46aa8e883
marcobusacca <marcobusacca01@gmail.com>
2023-07-11 14:00:00 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Head Title -->
+        <title>php-oop-3</title>
+    </head>
+    <body>
+    </body>
+</html>
