5023d91f3bc066eabcf202f4c23cf704042258ff
marcobusacca <marcobusacca01@gmail.com>
2023-07-11 21:53:09 +0200

    [FEAT] proprieta sotto-classe Sms

--- models/Sms.php
+++ models/Sms.php
@@ -1,5 +1,8 @@
 <?php
     class Sms extends CommunicationSystem{
-
+        // PROPRIETA
+        private $readNotification;
+        private $replyApproval;
+        public static $ledColor = "Green";
     }
 ?>
